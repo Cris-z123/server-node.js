67d1fb809a557f3a9a7f30102db4e5bf15281e33
ybZhang <472099249@qq.com>
2020-08-21 22:56:22 +0800

    init server

---
+++ public/black.js
@@ -0,0 +1,15 @@
+// const request = new XMLHttpRequest()
+// request.open('GET', 'http://localhost:8889/mock2.json')
+// request.onreadystatechange = () => {
+//     if(request.readyState === 4 && request.status === 200){
+//         console.log(request.response)
+//     }
+// }
+// request.send()
+
+
+// const script = document.createElement('script')
+// script.src = 'http://localhost:8889/jsonp.js'
+// document.body.appendChild(script)
+
+console.log('hello,i am hacker')
